ee569e0b82cf32d381a974df3f55b3e09d72b729
Keqi Chen <ckq009@gmail.com>
2023-04-14 16:11:26 +0100

    Add error image placeholder to the post list

--- myface-ui/src/Components/PostCard/PostCard.tsx
+++ myface-ui/src/Components/PostCard/PostCard.tsx
@@ -1,8 +1,8 @@
 ﻿import React from "react";
-import {Post} from "../../Api/apiClient";
-import {Card} from "../Card/Card";
+import { Post } from "../../Api/apiClient";
+import { Card } from "../Card/Card";
 import "./PostCard.scss";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface PostCardProps {
     post: Post;
@@ -12,10 +12,16 @@ export function PostCard(props: PostCardProps): JSX.Element {
     return (
         <Card>
             <div className="post-card">
-                <img className="image" src={props.post.imageUrl} alt=""/>
+                <img className="image"
+                    src={props.post.imageUrl}
+                    alt="This is a lovely"
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = "https://cdn.website.thryv.com/e0dd50daa5084ebdac62c38e03404c2c/dms3rep/multi/1197099-1.png";
+                    }} />
                 <div className="message">{props.post.message}</div>
                 <div className="user">
-                    <img className="profile-image" src={props.post.postedBy.profileImageUrl} alt=""/>
+                    <img className="profile-image" src={props.post.postedBy.profileImageUrl} alt="" />
                     <Link className="user-name" to={`/users/${props.post.postedBy.id}`}>{props.post.postedBy.displayName}</Link>
                 </div>
             </div>
